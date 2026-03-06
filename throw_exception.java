4e534e3bd5a3d45b3c7f16f3829393b0f4dbd5d4
azizbenabdessalem <azizbenabdessalem02@gmail.com>
2026-03-06 20:45:32 +0800

    Create throw_exception.java

---
+++ throw_exception.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        divide(10,0);
+    }
+
+    public static void divide(int a, int b) throws ArithmeticException {
+
+        if(b == 0){
+            throw new ArithmeticException("Division by zero");
+        }
+
+        System.out.println(a/b);
+    }
+}
+
+Ca arrete le code et ca donne un commentaire a un commentaire a des exceptions
