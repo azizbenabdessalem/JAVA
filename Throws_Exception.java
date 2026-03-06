ff3b3a0342ed7233b277810d2884c6e963fde88f
azizbenabdessalem <azizbenabdessalem02@gmail.com>
2026-03-06 20:42:58 +0800

    Create Throws_Exception.java

---
+++ Throws_Exception.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        divide();
+    }
+
+    public static void divide() throws ArithmeticException {
+        int x = 10 / 0;
+    }
+
+}
