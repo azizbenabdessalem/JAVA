286f8c5f6c8e81db058de970dbfa953797413758
azizbenabdessalem <azizbenabdessalem02@gmail.com>
2026-03-06 17:11:19 +0800

    Create try_catch.java

---
+++ try_catch.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        try  {
+            int a = 10 ;
+            int b = 0;
+
+            int result = a / b ;
+            System.out.println("Result : "+result );
+        } catch (ArithmeticException e) {
+            System.out.println("Ya bro , tu fous quoi la . Tu vries");
+        }
+
+    }
+} 
+
+La genre, quand il ya une erreur le programme ne s'arrete pas mais on va récupérrer l'erreur qui va aller dans le catch et apres on va afficher ce qu'il ya à l'intérieur du catch
+  
